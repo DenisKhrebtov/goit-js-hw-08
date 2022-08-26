e4c1dccd4d502514b991ca15d469167d0690331b
DenisKhrebtov <khrebtovdenis@gmail.com>
2022-08-26 23:32:41 +0300

    add 8.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const divRef = document.querySelector('.gallery');
+const cardsMarkup = createGallary(galleryItems);
+
+divRef.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGallary(items) {
+  return items
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" 
+            href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" 
+            />
+            </a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
