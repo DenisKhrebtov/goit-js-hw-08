03352dd36165fcc62216cb161d438231beb43379
DenisKhrebtov <khrebtovdenis@gmail.com>
2022-08-30 13:59:53 +0300

    hw 8.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,40 @@
 import throttle from 'lodash.throttle';
 
-const onEmail = document.querySelector('input');
-const onMessage = document.querySelector('textarea');
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  message: document.querySelector('textarea[name="message"]'),
+  email: document.querySelector('input[name="email"]'),
+};
 
-console.log(localStorage);
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+populateTextarea();
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  if (refs.email.value && refs.message.value) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  } else {
+    alert('Необходимо заполнить все поля');
+  }
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parsedSavedData = JSON.parse(savedMessage);
+  if (savedMessage) {
+    refs.email.value = parsedSavedData.email || '';
+    refs.message.value = parsedSavedData.message || '';
+  }
+}
